70a8383a37b1d5af59780f975b5cc692a8b01dbf
Ovi Stoica <ost@3e.eu>
2022-05-14 16:16:32 +0300

    Initial commit

---
+++ app/routes/notes/index.tsx
@@ -0,0 +1,21 @@
+import {Link} from '@remix-run/react'
+
+
+export function ErrorBoundary({error}: {error: Error}) {
+  return <p className={'text-red-700'}>
+    Something went wrong. Please retry
+    {' '}{error.message}
+  </p>
+}
+
+
+export default function NoteIndexPage() {
+  return (
+    <p>
+      No note selected. Select a note on the left, or{' '}
+      <Link to='new' className='text-blue-500 underline'>
+        create a new note.
+      </Link>
+    </p>
+  )
+}
